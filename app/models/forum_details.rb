1df1e118c9cdfc373d7071fdd9ce1db31a984064
Guy Molyneux <guy.molyneux@gmail.com>
2013-09-24 23:50:07 +0100

    made forum details non-mandatory

--- app/models/forum_details.rb
+++ app/models/forum_details.rb
@@ -1,5 +1,5 @@
 class ForumDetails < ActiveRecord::Base
 	belongs_to :member
-	validates :forum_id, numericality: true, uniqueness: true
-	validates :forum_name, length: {minimum: 2}, uniqueness: true
+	validates :forum_id, numericality: true, uniqueness: true, allow_nil: true, allow_blank: true
+	validates :forum_name, length: {minimum: 2}, uniqueness: true, allow_nil: true, allow_blank: true
 end
