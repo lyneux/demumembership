11daff7318e03b84c2d37181df2d8c3df99b0de6
Guy Molyneux <guy.molyneux@gmail.com>
2013-12-03 00:11:04 +0000

    Removed pound symbol which is supsected of breaking rake assets:precompile on openshift

--- app/models/member_category.rb
+++ app/models/member_category.rb
@@ -2,7 +2,7 @@ class MemberCategory < ActiveRecord::Base
 	has_many :member
 
 	def formatted_description
-		description + " (£" + (price_in_pence_per_year.to_i/100).to_s + " per year)"
+		description + " (" + (price_in_pence_per_year.to_i/100).to_s + " per year)"
 	end
 
 end
